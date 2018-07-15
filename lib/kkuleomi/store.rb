2dfedd16ddccff7d69cf0a01bcf2dd5425ef1b76
Bruno Pinto <brunoferreirapinto@gmail.com>
2018-07-15 15:28:32 +0100

    Convert tabs into spaces

--- lib/kkuleomi/store.rb
+++ lib/kkuleomi/store.rb
@@ -4,43 +4,43 @@ module Kkuleomi::Store
   end
 
   def self.create_index(force = false)
-		types = [
-			Category,
-			Package,
-			Version,
-			Change,
-			Useflag,
-		]
+    types = [
+      Category,
+      Package,
+      Version,
+      Change,
+      Useflag,
+    ]
 
     base_settings = {
-        analysis: {
-            filter: {
-                autocomplete_filter: {
-                    type: 'edge_ngram',
-                    min_gram: 1,
-                    max_gram: 20,
-                }
-            },
-            analyzer: {
-                autocomplete: {
-                    type: 'custom',
-                    tokenizer: 'standard',
-                    filter: %w(lowercase autocomplete_filter)
-                }
-            }
+      analysis: {
+        filter: {
+          autocomplete_filter: {
+            type: 'edge_ngram',
+            min_gram: 1,
+            max_gram: 20,
+          }
         },
-		mapping: { total_fields: { limit: 50000 } }
+        analyzer: {
+          autocomplete: {
+            type: 'custom',
+            tokenizer: 'standard',
+            filter: %w(lowercase autocomplete_filter)
+          }
+        }
+      },
+      mapping: { total_fields: { limit: 50000 } }
     }
 
-		# In ES 1.5, we could use 1 mega-index. But in ES6, each model needs its own.
-		types.each { |type|
-						client = type.gateway.client
-						client.indices.delete(index: type.index_name) rescue nil if force
-						body = {
-							settings: type.settings.to_hash.merge(base_settings),
-						 	mappings: type.mappings.to_hash
-						}
-						client.indices.create(index: type.index_name, body: body)
-		}
+    # In ES 1.5, we could use 1 mega-index. But in ES6, each model needs its own.
+    types.each { |type|
+      client = type.gateway.client
+      client.indices.delete(index: type.index_name) rescue nil if force
+      body = {
+        settings: type.settings.to_hash.merge(base_settings),
+        mappings: type.mappings.to_hash
+      }
+      client.indices.create(index: type.index_name, body: body)
+    }
   end
 end
